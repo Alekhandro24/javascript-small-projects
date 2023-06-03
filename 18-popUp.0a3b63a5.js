efb95ef94dbe60b43f4fc7978c108faf9430f589
Alekhandro24 <103840010+Alekhandro24@users.noreply.github.com>
2023-06-03 10:18:23 +0000

    Deploying to gh-pages from @ Alekhandro24/javascript-small-projects@e9705092e8cd8b9f4c9fcbfec09b739cdbb6cb9a 🚀

---
+++ 18-popUp.0a3b63a5.js
@@ -0,0 +1,2 @@
+const submitBtnEl=document.querySelector(".submitBtn"),okBtn=document.querySelector(".okBtn"),popUpEl=document.querySelector(".pop-up");submitBtnEl.addEventListener("click",(()=>{popUpEl.classList.add("pop-up-open")})),okBtn.addEventListener("click",(()=>{popUpEl.classList.remove("pop-up-open")}));
+//# sourceMappingURL=18-popUp.0a3b63a5.js.map
