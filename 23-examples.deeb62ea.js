7a087de9e94260a14b67e89cccfa945ead9e55d9
Alekhandro24 <103840010+Alekhandro24@users.noreply.github.com>
2023-06-08 13:08:22 +0000

    Deploying to gh-pages from @ Alekhandro24/javascript-small-projects@afc763a43c3ac8d1dbff88ea920796fff80511ea 🚀

---
+++ 23-examples.deeb62ea.js
@@ -0,0 +1,2 @@
+const data=[100,200,500,300,5,1];function getLargestNumber(o){const t=data.sort(((o,t)=>o-t));console.log(t);const e=t.length;return console.log(e),t[e-o]}console.log(getLargestNumber(6));
+//# sourceMappingURL=23-examples.deeb62ea.js.map
