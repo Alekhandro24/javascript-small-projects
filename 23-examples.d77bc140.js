92960f4ac3675703eb0254249e3adb4f9d58de6b
Alekhandro24 <103840010+Alekhandro24@users.noreply.github.com>
2023-06-12 12:18:48 +0000

    Deploying to gh-pages from @ Alekhandro24/javascript-small-projects@1ddce3120f01dc02b2dd1e86cff081a1ccc00f3f 🚀

---
+++ 23-examples.d77bc140.js
@@ -0,0 +1,2 @@
+class Person{constructor(e,s){this.name=e,this.age=s}}const adam=new Person("Adam",15),eve=new Person("Eve",18);class Vehicle{hasEngine=!0}class Bus extends Vehicle{}class Boat extends Vehicle{}const a=new Bus;console.log("a.hasEngine",a.hasEngine);const b=new Boat;console.log("b.hasEngine",b.hasEngine);
+//# sourceMappingURL=23-examples.d77bc140.js.map
