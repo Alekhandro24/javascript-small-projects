420ffd544603db8f10eaa9415ca4fc9650a73585
Alekhandro24 <103840010+Alekhandro24@users.noreply.github.com>
2023-06-16 18:19:48 +0000

    Deploying to gh-pages from @ Alekhandro24/javascript-small-projects@da8cce61f644059602460dd84a18c25f77e5d032 🚀

---
+++ 23-examples.d65a0b19.js
@@ -0,0 +1,2 @@
+class Person{constructor(e,s){this.name=e,this.age=s}}const adam=new Person("Adam",15),eve=new Person("Eve",18),numbers=[5,8,1,3,4,7,9],replaceWithNumbers=numbers.with(3,6);console.log("replaceWithNumbers",replaceWithNumbers),console.log("numbers",numbers);
+//# sourceMappingURL=23-examples.d65a0b19.js.map
