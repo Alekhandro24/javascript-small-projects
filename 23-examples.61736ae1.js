78476dca117db6b143be3e3b524dc64fde396cca
Alekhandro24 <103840010+Alekhandro24@users.noreply.github.com>
2023-06-18 19:46:14 +0000

    Deploying to gh-pages from @ Alekhandro24/javascript-small-projects@a8f74816904be68e3f5595ffe4fb9a2147e00ef6 🚀

---
+++ 23-examples.61736ae1.js
@@ -0,0 +1,2 @@
+class Person{constructor(e,s){this.name=e,this.age=s}}const adam=new Person("Adam",15),eve=new Person("Eve",18),numbers=[5,8,1,3,4,7,9];
+//# sourceMappingURL=23-examples.61736ae1.js.map
---
+++ 26-tic-tac-toe2.7b13a421.js
@@ -0,0 +1,2 @@
+const cellElements=document.querySelectorAll("[data-cell]");function handleClick(e){}cellElements.forEach((e=>{e.addEventListener("click",handleClick,{once:!0})}));
+//# sourceMappingURL=26-tic-tac-toe2.7b13a421.js.map
