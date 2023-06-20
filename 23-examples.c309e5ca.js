dc9e0bcce0ca8152e847a23bb427ac8f7ae584ae
Alekhandro24 <103840010+Alekhandro24@users.noreply.github.com>
2023-06-20 12:15:05 +0000

    Deploying to gh-pages from @ Alekhandro24/javascript-small-projects@6f192255f7a8013b28f4a950f8fa7408a1011d66 🚀

---
+++ 23-examples.c309e5ca.js
@@ -0,0 +1,2 @@
+class Person{constructor(e,t){this.name=e,this.age=t}}const adam=new Person("Adam",15),eve=new Person("Eve",18),countries=document.getElementById("countries"),countryCapital=document.getElementById("capital");function choosenCountryCapital(){countryCapital.value=countries.value}countries.addEventListener("click",choosenCountryCapital);
+//# sourceMappingURL=23-examples.c309e5ca.js.map
