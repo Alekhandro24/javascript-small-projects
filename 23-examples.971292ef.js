d02bf2e033e49c287b85b4aa0ea0a0169e8b0770
Alekhandro24 <103840010+Alekhandro24@users.noreply.github.com>
2023-06-24 08:21:31 +0000

    Deploying to gh-pages from @ Alekhandro24/javascript-small-projects@0ab74914dc25c302713173f8bfb2a9cd3e583a79 🚀

---
+++ 23-examples.971292ef.js
@@ -0,0 +1,2 @@
+class Person{constructor(e,s){this.name=e,this.age=s}}const adam=new Person("Adam",15),eve=new Person("Eve",18);let array=[3,45,84,56,1,98,20];function sumTwoSmallestNumbers(e){let s=e.sort(((e,s)=>e-s)).slice(0,2).reduce(((e,s)=>e+s));console.log("r: ",s)}sumTwoSmallestNumbers(array);
+//# sourceMappingURL=23-examples.971292ef.js.map
