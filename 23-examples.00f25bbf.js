1cb64d7c991a875a91166c7159325832c5bd7d3c
Alekhandro24 <103840010+Alekhandro24@users.noreply.github.com>
2023-06-27 13:10:16 +0000

    Deploying to gh-pages from @ Alekhandro24/javascript-small-projects@76e57724e70ed954a28d10f03227bdabe65b31db 🚀

---
+++ 23-examples.00f25bbf.js
@@ -0,0 +1,2 @@
+class Person{constructor(e,t){this.name=e,this.age=t}}const adam=new Person("Adam",15),eve=new Person("Eve",18);function bubbleSort(e){for(let t=0;t<e.length;t++)for(let o=0;o<e.length-1-t;o++)if(e[o]>e[o+1]){const t=e[o];e[o]=e[o+1],e[o+1]=t}console.log("arr",e)}const test=[33,1,15,122,5,245,65,90,23];bubbleSort(test);
+//# sourceMappingURL=23-examples.00f25bbf.js.map
